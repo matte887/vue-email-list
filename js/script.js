d7cd52428ef72b27cd0ae73ca835a1b1ecd184fd
matte887 <m.erroi92@gmail.com>
2022-05-13 15:34:51 +0200

    Stampate mail random

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        numberOfMail: 10,
+        thisMail: '',
+        arrayMails: []
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((result) => {
+                this.thisMail = result.data.response;
+                this.arrayMails.push(this.thisMail);
+                console.log(this.arrayMails);
+            });                
+        }
+    }
+});
